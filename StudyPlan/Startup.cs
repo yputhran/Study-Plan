361fc84452183536a4137f8e71c40ad6e82da140
Yashila Puthran <yashilaputhran@Yashilas-MacBook.local>
2017-08-12 13:49:58 -0400

    First Commit

---
+++ StudyPlan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudyPlan.Startup))]
+namespace StudyPlan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
